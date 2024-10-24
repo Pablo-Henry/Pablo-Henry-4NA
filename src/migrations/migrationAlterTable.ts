4a06d10700022e9cba8d95e633c827292e267c02
Pablo  Henry <35946059-pablohenryfaria@users.noreply.replit.com>
2024-10-24 02:42:08 +0000

    chore: adicionei um sistema de validação de senha e criação de senha criptografada utilizando o passwordhash

--- src/migrations/migrationAlterTable.ts
+++ src/migrations/migrationAlterTable.ts
@@ -4,11 +4,8 @@ const createUsersTable = async () => {
   const client = await pool.connect();
   try {
     const queryText = `
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL
-      );
+      ALTER TABLE IF NOT EXISTS users
+      ADD COLUMN IF NOT EXISTS password_hash VARCHAR(100);
     `;
     await client.query(queryText);
     console.log('Tabela "users" criada com sucesso!');
